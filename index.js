cf47ee8818b2f99ff1e75af145902e655450f400
Jakub Dolezal <jakub@dolez.al>
2017-07-21 12:50:43 +0200

    remove translation

--- index.js
+++ index.js
@@ -18,13 +18,8 @@
 'use strict';
 
 var forEach = require('for-each');
-var warning = require('warning');
 var has = require('has');
 var trim = require('string.prototype.trim');
-
-var warn = function warn(message) {
-  warning(false, message);
-};
 
 var replace = String.prototype.replace;
 var split = String.prototype.split;
@@ -152,118 +147,9 @@ function transformPhrase(phrase, substitutions, locale) {
 }
 
 // ### Polyglot class constructor
-function Polyglot(options) {
-  var opts = options || {};
-  this.phrases = {};
-  this.extend(opts.phrases || {});
-  this.currentLocale = opts.locale || 'en';
-  var allowMissing = opts.allowMissing ? transformPhrase : null;
-  this.onMissingKey = typeof opts.onMissingKey === 'function' ? opts.onMissingKey : allowMissing;
-  this.warn = opts.warn || warn;
+function Polyglot() {
 }
 
-// ### polyglot.locale([locale])
-//
-// Get or set locale. Internally, Polyglot only uses locale for pluralization.
-Polyglot.prototype.locale = function (newLocale) {
-  if (newLocale) this.currentLocale = newLocale;
-  return this.currentLocale;
-};
-
-// ### polyglot.extend(phrases)
-//
-// Use `extend` to tell Polyglot how to translate a given key.
-//
-//     polyglot.extend({
-//       "hello": "Hello",
-//       "hello_name": "Hello, %{name}"
-//     });
-//
-// The key can be any string.  Feel free to call `extend` multiple times;
-// it will override any phrases with the same key, but leave existing phrases
-// untouched.
-//
-// It is also possible to pass nested phrase objects, which get flattened
-// into an object with the nested keys concatenated using dot notation.
-//
-//     polyglot.extend({
-//       "nav": {
-//         "hello": "Hello",
-//         "hello_name": "Hello, %{name}",
-//         "sidebar": {
-//           "welcome": "Welcome"
-//         }
-//       }
-//     });
-//
-//     console.log(polyglot.phrases);
-//     // {
-//     //   'nav.hello': 'Hello',
-//     //   'nav.hello_name': 'Hello, %{name}',
-//     //   'nav.sidebar.welcome': 'Welcome'
-//     // }
-//
-// `extend` accepts an optional second argument, `prefix`, which can be used
-// to prefix every key in the phrases object with some string, using dot
-// notation.
-//
-//     polyglot.extend({
-//       "hello": "Hello",
-//       "hello_name": "Hello, %{name}"
-//     }, "nav");
-//
-//     console.log(polyglot.phrases);
-//     // {
-//     //   'nav.hello': 'Hello',
-//     //   'nav.hello_name': 'Hello, %{name}'
-//     // }
-//
-// This feature is used internally to support nested phrase objects.
-Polyglot.prototype.extend = function (morePhrases, prefix) {
-  forEach(morePhrases, function (phrase, key) {
-    var prefixedKey = prefix ? prefix + '.' + key : key;
-    if (typeof phrase === 'object') {
-      this.extend(phrase, prefixedKey);
-    } else {
-      this.phrases[prefixedKey] = phrase;
-    }
-  }, this);
-};
-
-// ### polyglot.unset(phrases)
-// Use `unset` to selectively remove keys from a polyglot instance.
-//
-//     polyglot.unset("some_key");
-//     polyglot.unset({
-//       "hello": "Hello",
-//       "hello_name": "Hello, %{name}"
-//     });
-//
-// The unset method can take either a string (for the key), or an object hash with
-// the keys that you would like to unset.
-Polyglot.prototype.unset = function (morePhrases, prefix) {
-  if (typeof morePhrases === 'string') {
-    delete this.phrases[morePhrases];
-  } else {
-    forEach(morePhrases, function (phrase, key) {
-      var prefixedKey = prefix ? prefix + '.' + key : key;
-      if (typeof phrase === 'object') {
-        this.unset(phrase, prefixedKey);
-      } else {
-        delete this.phrases[prefixedKey];
-      }
-    }, this);
-  }
-};
-
-// ### polyglot.clear()
-//
-// Clears all phrases. Useful for special cases, such as freeing
-// up memory if you have lots of phrases but no longer need to
-// perform any translation. Also used internally by `replace`.
-Polyglot.prototype.clear = function () {
-  this.phrases = {};
-};
 
 // ### polyglot.replace(phrases)
 //
@@ -273,52 +159,6 @@ Polyglot.prototype.clear = function () {
 Polyglot.prototype.replace = function (newPhrases) {
   this.clear();
   this.extend(newPhrases);
-};
-
-
-// ### polyglot.t(key, options)
-//
-// The most-used method. Provide a key, and `t` will return the
-// phrase.
-//
-//     polyglot.t("hello");
-//     => "Hello"
-//
-// The phrase value is provided first by a call to `polyglot.extend()` or
-// `polyglot.replace()`.
-//
-// Pass in an object as the second argument to perform interpolation.
-//
-//     polyglot.t("hello_name", {name: "Spike"});
-//     => "Hello, Spike"
-//
-// If you like, you can provide a default value in case the phrase is missing.
-// Use the special option key "_" to specify a default.
-//
-//     polyglot.t("i_like_to_write_in_language", {
-//       _: "I like to write in %{language}.",
-//       language: "JavaScript"
-//     });
-//     => "I like to write in JavaScript."
-//
-Polyglot.prototype.t = function (key, options) {
-  var phrase, result;
-  var opts = options == null ? {} : options;
-  if (typeof this.phrases[key] === 'string') {
-    phrase = this.phrases[key];
-  } else if (typeof opts._ === 'string') {
-    phrase = opts._;
-  } else if (this.onMissingKey) {
-    var onMissingKey = this.onMissingKey;
-    result = onMissingKey(key, opts, this.currentLocale);
-  } else {
-    this.warn('Missing translation for key: "' + key + '"');
-    result = key;
-  }
-  if (typeof phrase === 'string') {
-    result = transformPhrase(phrase, opts, this.currentLocale);
-  }
-  return result;
 };
 
 
